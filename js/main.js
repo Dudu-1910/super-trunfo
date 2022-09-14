b3d3fdeafa6a34365f21ae1f57fe103f594b0c46
Eduardo Henrique Vieira dos Santos <94689304+Dudu-1910@users.noreply.github.com>
2022-09-14 17:03:24 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,487 @@
+const cartaA1 = {
+  nome: 'Granada Ghia 3.0',
+  imagem: 'https://i.pinimg.com/564x/ca/58/16/ca58161041284b65f11f927e4aac1760.jpg',
+  atributos: {
+    kmH: 180,
+    hp: 138,
+    zeroACem: '11seg',
+    cc: 2993,
+    cilindros: 'V6',
+    kg: 1365
+  }
+}
+
+const cartaA2 = {
+  nome: 'Diplomat v8',
+  imagem: 'https://i.pinimg.com/564x/97/c0/2c/97c02cfb5503cc66be54bed8baee9a83.jpg',
+  atributos: {
+    kmH: 205,
+    hp: 230,
+    zeroACem: '10seg',
+    cc: 5354,
+    cilindros: 'V8',
+    kg: 1690
+  }
+}
+
+const cartaA3 = {
+  nome: 'BMW 3.3 Li',
+  imagem: 'https://i.pinimg.com/564x/e6/da/e0/e6dae085a365ca51d36c3b5602dc95f7.jpg',
+  atributos: {
+    kmH: 210,
+    hp: 200,
+    zeroACem: '9seg',
+    cc: 3295,
+    cilindros: 6,
+    kg: 1350
+  }
+}
+
+const cartaA4 = {
+  nome: 'Mercedes 450 SEL 6.9',
+  imagem: 'https://i.pinimg.com/564x/ef/27/2c/ef272cf78af233ac2db34fd221539ed4.jpg',
+  atributos: {
+    kmH: 225,
+    hp: 286,
+    zeroACem: '8seg',
+    cc: 6834,
+    cilindros: 'V8',
+    kg: 1935
+  }
+}
+
+const cartaB1 = {
+  nome: 'Rolls-Royce Camargue',
+  imagem: 'https://i.pinimg.com/564x/d9/ea/25/d9ea25c8bdad4b321ae5d98d6aabecf5.jpg',
+  atributos: {
+    kmH: 190,
+    hp: 200,
+    zeroACem: '12seg',
+    cc: 6750,
+    cilindros: 'V8',
+    kg: 2347
+  }
+}
+
+const cartaB2 = {
+  nome: 'Rover 3500 S',
+  imagem: 'https://i.pinimg.com/564x/53/a3/e3/53a3e35875ce07f4ed0bd593810d54ad.jpg',
+  atributos: {
+    kmH: 190,
+    hp: 141,
+    zeroACem: '11seg',
+    cc: 3470,
+    cilindros: 'V8',
+    kg: 1360
+ }
+}
+
+const cartaB3 = {
+  nome: 'Lotus Elite 502',
+  imagem: 'https://i.pinimg.com/564x/b0/10/30/b01030a223bfa97c92afb71f2baf9a4b.jpg',
+  atributos: {
+    kmH: 200,
+    hp: 150,
+    zeroACem: '8seg',
+    cc: 1973,
+    cilindros: 4,
+    kg: 1060
+  }
+}
+
+const cartaB4 = {
+  nome: 'Jaguar XJ-S',
+  imagem: 'https://i.pinimg.com/564x/50/0e/3f/500e3f6a2b9840b7a4416fe44870fecc.jpg',
+  atributos: {
+    kmH: 240,
+    hp: 287,
+    zeroACem: '7seg',
+    cc: 5343,
+    cilindros: 'V12',
+    kg: 1760
+  }
+}
+
+const cartaC1 = {
+  nome: 'Alfa Romeo 2000 Spider',
+  imagem: 'https://i.pinimg.com/564x/15/08/34/15083488a004d4945b38435bdd197c00.jpg',
+  atributos: {
+    kmH: 198,
+    hp: 131,
+    zeroACem: '11seg',
+    cc: 1962,
+    cilindros: 4,
+    kg: 1045
+  }
+}
+
+const cartaC2 = {
+  nome: 'Mercedes 450 SL',
+  imagem: 'https://i.pinimg.com/564x/17/6e/d8/176ed8bda3edc1e4c7cc506acdfc3416.jpg',
+  atributos: {
+    kmH: 215,
+    hp: 225,
+    zeroACem: '9seg',
+    cc: 4520,
+    cilindros: 'V8',
+    kg: 1580
+  }
+}
+
+const cartaC3 = {
+  nome: 'Panther J 72 V 12',
+  imagem: 'https://i.pinimg.com/564x/3a/f6/a3/3af6a3b62bbc9af0229cef4f07623574.jpg',
+  atributos: {
+    kmH: 220,
+    hp: 269,
+    zeroACem: '6seg',
+    cc: 5343,
+    cilindros: 'V12',
+    kg: 1350
+  }
+}
+
+const cartaC4 = {
+  nome: 'Monteverdi Palm Beach',
+  imagem: 'https://i.pinimg.com/564x/89/e3/e4/89e3e4244cbcfa2a8e4acce504dd2207.jpg',
+  atributos: {
+    kmH: 240,
+    hp: 305,
+    zeroACem: '6seg',
+    cc: 7207,
+    cilindros: 'V8',
+    kg: 1430
+  }
+}
+
+const cartaD1 = {
+  nome: 'Buick Century',
+  imagem: 'https://i.pinimg.com/564x/f7/44/dd/f744dd60a5ee18a055c5386fd18b4339.jpg',
+  atributos: {
+    kmH: 170,
+    hp: 165,
+    zeroACem: '12seg',
+    cc: 5665,
+    cilindros: 'V8',
+    kg: 1860
+  }
+}
+
+const cartaD2 = {
+  nome: 'Chevrolet Camaro LT',
+  imagem: 'https://i.pinimg.com/564x/9a/ef/c4/9aefc4d1019d08216fda75a297f619dd.jpg',
+  atributos: {
+    kmH: 180,
+    hp: 155,
+    zeroACem: '12seg',
+    cc: 5657,
+    cilindros: 'V8',
+    kg: 1690
+  }
+}
+
+const cartaD3 = {
+  nome: 'Oldsmobile 98 Regency',
+  imagem: 'https://i.pinimg.com/564x/15/25/74/152574e06ec619c27d411b897a843d81.jpg',
+  atributos: {
+    kmH: 195,
+    hp: 190,
+    zeroACem: '11seg',
+    cc: 7325,
+    cilindros: 'V8',
+    kg: 2330
+  }
+}
+
+const cartaD4 = {
+  nome: 'Pontiac Firebird Formula',
+  imagem: 'https://i.pinimg.com/564x/3a/45/51/3a455171ae370403942dfd140556797c.jpg',
+  atributos: {
+    kmH: 200,
+    hp: 185,
+    zeroACem: '10seg',
+    cc: 6473,
+    cilindros: 'V8',
+    kg: 1745
+  }
+}
+
+const cartaE1 = {
+  nome: 'Alfetta GT',
+  imagem: 'https://i.pinimg.com/564x/a4/e9/ec/a4e9ec0b854cab84cfc86f2c7e64a0d4.jpg',
+  atributos: {
+    kmH: 190,
+    hp: 121,
+    zeroACem: '11seg',
+    cc: 1779,
+    cilindros: 4,
+    kg: 1080
+  }
+}
+
+const cartaE2 = {
+  nome: 'Alfa Romeo Montreal',
+  imagem: 'https://i.pinimg.com/564x/ad/51/22/ad5122788981d643eb08305c4b3f3693.jpg',
+  atributos: {
+    kmH: 220,
+    hp: 195,
+    zeroACem: '8seg',
+    cc: 2593,
+    cilindros: 'V8',
+    kg: 1310
+  }
+}
+
+const cartaE3 = {
+  nome: 'Lancia Stratos',
+  imagem: 'https://i.pinimg.com/564x/64/74/1d/64741dfbf5ee1a5a9882f8da906f4284.jpg',
+  atributos: {
+    kmH: 230,
+    hp: 190,
+    zeroACem: '7seg',
+    cc: 2418,
+    cilindros: 'V6',
+    kg: 870
+  }
+}
+
+const cartaE4 = {
+  nome: 'Ferrari 365 GT/4',
+  imagem: 'https://i.pinimg.com/564x/96/99/fd/9699fd104509d95e6d12c4c0ab156c31.jpg',
+  atributos: {
+    kmH: 250,
+    hp: 340,
+    zeroACem: '6seg',
+    cc: 4390,
+    cilindros: 'V12',
+    kg: 1500
+  }
+}
+
+const cartaF1 = {
+  nome: 'Citroen CX 2200',
+  imagem: 'https://i.pinimg.com/564x/b3/23/05/b32305905a216123554d459f0705d41e.jpg',
+  atributos: {
+    kmH: 180,
+    hp: 112,
+    zeroACem: '13seg',
+    cc: 2175,
+    cilindros: 4,
+    kg: 1280
+  }
+}
+
+const cartaF2 = {
+  nome: 'Renault 16 TX',
+  imagem: 'https://i.pinimg.com/564x/62/00/74/62007449c3cd17988788513ae3f700cc.jpg',
+  atributos: {
+    kmH: 170,
+    hp: 93,
+    zeroACem: '13seg',
+    cc: 1647,
+    cilindros: 4,
+    kg: 1100
+  }
+}
+
+const cartaF3 = {
+  nome: 'Renault 30 TS',
+  imagem: 'https://i.pinimg.com/564x/a9/5d/f1/a95df17dcbb43266335a1af89b9298da.jpg',
+  atributos: {
+    kmH: 185,
+    hp: 131,
+    zeroACem: '11seg',
+    cc: 2664,
+    cilindros: 'V6',
+    kg: 1320
+  }
+}
+
+const cartaF4 = {
+  nome: 'Peugeot 604 V 6 SL',
+  imagem: 'https://i.pinimg.com/564x/4d/be/43/4dbe43fb881f51b27825f40f020d882a.jpg',
+  atributos: {
+    kmH: 180,
+    hp: 136,
+    zeroACem: '11seg',
+    cc: 2664,
+    cilindros: 'V6',
+    kg: 1480
+  }
+}
+
+const cartaG1 = {
+  nome: 'Porsche Turbo',
+  imagem: 'https://i.pinimg.com/564x/ab/18/8f/ab188fb8b2c63fcc62865467197811ee.jpg',
+  atributos: {
+    kmH: 250,
+    hp: 260,
+    zeroACem: '5seg',
+    cc: 2992,
+    cilindros: 6,
+    kg: 1140
+  }
+}
+
+const cartaG2 = {
+  nome: 'Monteverdi Hai 450 GTS',
+  imagem: 'https://i.pinimg.com/564x/a7/ca/7e/a7ca7ee12ab4ddf80949b10de0f1d54b.jpg',
+  atributos: {
+    kmH: 290,
+    hp: 450,
+    zeroACem: '5seg',
+    cc: 6978,
+    cilindros: 'V8',
+    kg: 1290
+  }
+}
+
+const cartaG4 = {
+  nome: 'Lamborghini Countach',
+  imagem: 'https://i.pinimg.com/564x/cd/f9/2c/cdf92c894a2a571d0899d8e2f8e90ad5.jpg',
+  atributos: {
+    kmH: 310,
+    hp: 385,
+    zeroACem: '5seg',
+    cc: 3929,
+    cilindros: 'V12',
+    kg: 1100
+  }
+}
+
+const cartaH1 = {
+  nome: 'Fiat 130 Coupé',
+  imagem: 'https://i.pinimg.com/564x/4e/f5/85/4ef58577118ed15ef241271fea37a84b.jpg',
+  atributos: {
+    kmH: 180,
+    hp: 165,
+    zeroACem: '12seg',
+    cc: 3235,
+    cilindros: 'V6',
+    kg: 1600
+  }
+}
+
+const cartaH2 = {
+  nome: 'Peugeot 504 V 6 Coupé',
+  imagem: 'https://i.pinimg.com/564x/50/95/fe/5095fe1b394596b9700d1152421c81ac.jpg',
+  atributos: {
+    kmH: 190,
+    hp: 136,
+    zeroACem: '11seg',
+    cc: 2664,
+    cilindros: 'V6',
+    kg: 1300
+  }
+}
+
+const cartaH3 = {
+  nome: 'Mercedes 280 CE',
+  imagem: 'https://i.pinimg.com/564x/57/36/4c/57364c94b3a40ea57d12e4297a9bfb0b.jpg',
+  atributos: {
+    kmH: 200,
+    hp: 185,
+    zeroACem: '10seg',
+    cc: 2746,
+    cilindros: 6,
+    kg: 1455
+  }
+}
+
+const cartaH4 = {
+  nome: 'Monteverdi 375 L',
+  imagem: 'https://i.pinimg.com/564x/4d/be/43/4dbe43fb881f51b27825f40f020d882a.jpg',
+  atributos: {
+    kmH: 240,
+    hp: 305,
+    zeroACem: '7seg',
+    cc: 7207,
+    cilindros: 'V8',
+    kg: 1600 
+  }
+}
+
+
+const cartas = [cartaA1, cartaA2, cartaA3, cartaA4, cartaB1, cartaB2, cartaB3, cartaB4, cartaC1, cartaC2, cartaC3, cartaC4, cartaD1, cartaD2, cartaD3, cartaD4, cartaE1, cartaE2, cartaE3, cartaE4, cartaF1, cartaF2, cartaF3, cartaF4, cartaG1, cartaG2, cartaG4, cartaH1, cartaH2, cartaH3, cartaH4];
+
+let cartaMaquina;
+let cartaJogador;
+
+
+function sortearCarta() {
+  let numeroCartaMaquina = parseInt(Math.random() * 31);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  let numeroCartaJogador = parseInt(Math.random() * 31);
+  while (numeroCartaMaquina === numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * 31);
+  }
+
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+
+  document.querySelector("#btn-sortear").disabled = true;
+  document.querySelector("#btn-jogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  let radioAtributos = document.getElementsByName("atributo");
+
+  for (let i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked === true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  let atributoSelecionado = obtemAtributoSelecionado();
+  let divResultado = document.querySelector("#resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'> Venceu </p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'> Perdeu </p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'> Empatou </p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.querySelector("#btn-jogar").disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  let divCartaJogador = document.querySelector("#carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  let moldura ='<img src="./img/card-super-trunfo-transparent-ajustado.png" style="width: inherit; height: inherit; position: absolute;">';
+  let tagHTML = '<div id="opcoes" class="carta-status">';
+
+  let opcoesTexto = "";
+  for (let atributo in cartaJogador.atributos) {
+    opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>";
+  }
+  let nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  let divCartaMaquina = document.querySelector("#carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  let moldura = '<img src="./img/card-super-trunfo-transparent-ajustado.png" style="width: inherit; height: inherit; position: absolute;">';
+  let tagHTML = '<div id="opcoes" class="carta-status">';
+
+  let opcoesTexto = "";
+  for (let atributo in cartaMaquina.atributos) {
+    opcoesTexto += "<span class='span-carta-maquina' type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "</span><br>";
+  }
+  let nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
